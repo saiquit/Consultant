2964dc2971e0a4d703250a18af9a64ceab8f62ef
saiquit <imamhossain130754@gmail.com>
2022-12-20 22:01:24 -0800

    updated admin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -28,7 +28,7 @@ class UserSeeder extends Seeder
         }
         \App\Models\User::factory([
             'name' => 'admin',
-            'email' => 'admin@admin.com',
+            'email' => 'imamhossain130754@gmail.com',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'type'  => 'admin'
         ])->create();
